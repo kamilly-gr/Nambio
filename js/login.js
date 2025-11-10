1c9422031a31a4d13f798db155c90655f5157b02
kamilly-gr <kazim.riber@gmail.com>
2025-11-10 10:03:41 -0300

    atualizações na página de login e de contato

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function mostrarSenha(){
+    let botao = document.getElementById("botao-senha")
+    let campo = document.getElementById("senha")
+
+    if(campo.type === "password"){
+        campo.type = "text"
+        botao.style.backgroundImage = "url(/Nambio/assets/icons/olho-senha)"
+    } else{
+        campo.type = "password"
+        botao.style.backgroundImage = "url(/Nambio/assets/icons/olho-senha-fechado)"
+    }
+
+    botao.style.backgroundRepeat = "no-repeat";
+    botao.style.backgroundPosition = "center";
+    botao.style.backgroundSize = "contain";
+}
