9992c370571cc8d2ad17ed83e9b543548530a121
LiviaStockl <liviaastockl@gmail.com>
2025-11-10 16:24:21 -0300

    edições

--- js/cadastro.js
+++ js/cadastro.js
@@ -9,20 +9,20 @@ function avançarParaEndereco() {
   }
 
   // Esconde o primeiro container e mostra o segundo
-  document.getElementById('cadastroContainer1').style.display = 'none';
-  document.getElementById('cadastroContainer2').style.display = 'block';
+  document.getElementById('ctnLoginAluno 1').style.display = 'none';
+  document.getElementById('ctnLoginAluno 2').style.display = 'block';
 }
 
 function voltarParaUsuario() {
   // Volta para a primeira etapa
-  document.getElementById('cadastroContainer2').style.display = 'none';
-  document.getElementById('cadastroContainer1').style.display = 'block';
+  document.getElementById('ctnLoginAluno 2').style.display = 'none';
+  document.getElementById('ctnLoginAluno 1').style.display = 'block';
 }
 
 function finalizarCadastro() {
   // Aqui você pode coletar todos os dados e enviar
 
-  
+
   const dados = {
     nome: document.getElementById('nome').value,
     email: document.getElementById('email').value,
