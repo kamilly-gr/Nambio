7170ec0c27193648ea1e5fa00fc4b6ca5b15c404
elisagommes <elisagommes@outlook.com>
2025-12-10 10:34:21 -0300

    ajustes finais para a apresentação

--- js/exemploDeCasa.js
+++ js/exemploDeCasa.js
@@ -15,6 +15,15 @@ document.addEventListener('DOMContentLoaded', function () {
         function confirmacao(){
             let conhecer = document.getElementById('conhecer-familia')
             let confirmacao = document.getElementById('COnfirmacao-tag')
+            let noti = document.getElementById('noti1', 'noti2')
+            let relato = document.getElementById('relato')
+            let email = document.getElementById('email')
+            let nome = document.getElementById('nome')
+
+            noti.checked = false
+            relato.value = null
+            email.value = null
+            nome.value = null
 
             conhecer.style.display = 'none'
 
